fcb64ab777f7b216d4638a42fc4630e871002d5a
Yaroslav Viktorov <78563098+ViktorovYaroslav@users.noreply.github.com>
2022-07-18 17:13:21 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,15 +4,16 @@ let rgbCol = 255;
 let button = document.querySelector('button');
 
 function randomCol(num){
-   let color;
-   return color = Math.floor(Math.random() * (num + 1));
+   return Math.floor(Math.random() * (num + 1));
    
 }
 
-button.onclick = function(){
+function change(){
    let color = 'rgb(' + randomCol(rgbCol) + ',' + randomCol(rgbCol) + ','  + randomCol(rgbCol) + ')';
    document.body.style.backgroundColor = color;
 
    let p = document.querySelector('p');
    p.innerText = color;
 }
+
+button.addEventListener('click', change);
